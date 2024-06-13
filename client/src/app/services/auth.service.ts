05465b52345879a80cd9097b8298f776ea444e2b
Arthur <94467283+avlrd@users.noreply.github.com>
2024-06-13 08:59:58 +0000

    Je suis perdu

--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -1,19 +1,37 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { JwtHelperService } from "@auth0/angular-jwt";
+import { tap } from "rxjs/operators";
+import { Observable } from "rxjs";
 
 import Credentials from '../models/credentials.model';
+import { tokenGetter } from "../app.config";
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
-	constructor(private http: HttpClient) {}
+	constructor(private http: HttpClient, private jwtHelper: JwtHelperService) {}
 
 	register(credentials: Credentials) {
 		console.log("Register request");
-		//return this.http.post<any>('/api/auth/register', credentials);
+		return this.http.post<any>('/api/auth/register', credentials);
 	}
 
 	login(credentials: Credentials) {
 		console.log("Login request");
-		//return this.http.post<any>('/api/auth/login', credentials);
+		return this.http.post<any>('/api/auth/login', credentials)
+		.pipe(
+			tap(response => {
+				localStorage.setItem('access_token', response.token);
+			})
+		);
+	}
+
+	logout(): void {
+		localStorage.removeItem('access_token');
+	}
+
+	isAuthenticated(): boolean {
+		const token = tokenGetter();
+		return token ? !this.jwtHelper.isTokenExpired(token) : false;
 	}
 }
--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -2,16 +2,19 @@ import { Request, Response } from 'express';
 import { hashPassword, comparePasswords } from '../tools/bcrypt.tools';
 import { generateToken } from '../tools/jwt.tools';
 import User from '../models/user.model';
+import Credentials from '../models/credentials.model';
 
 export const register = async (request: Request, response: Response) => {
-	const { username, password } = request.body;
+	console.log("Register request");
+	const credentials: Credentials = request.body;
 
 	try {
+		let username = credentials.username;
 		let user = await User.findOne ({ username });
 
 		if (user) return response.status(400).json({ message: 'User already exists' });
 
-		const hashedPassword = await hashPassword(password);
+		const hashedPassword = await hashPassword(credentials.password);
 
 		user = new User({
 			username,
@@ -30,14 +33,15 @@ export const register = async (request: Request, response: Response) => {
 };
 
 export const login = async (request: Request, response: Response) => {
-	const { username, password } = request.body;
+	const credentials: Credentials = request.body;
 
 	try {
+		const username = credentials.username;
 		const user = await User.findOne({ username });
 
 		if (!user) return response.status(400).json({ message: 'User not found' });
 
-		const passwordMatch = await comparePasswords(password, user.password);
+		const passwordMatch = await comparePasswords(credentials.password, user.password);
 
 		if (!passwordMatch) return response.status(400).json({ message: 'Invalid credentials' });
 
