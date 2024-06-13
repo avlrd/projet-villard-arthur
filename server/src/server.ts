875605d4842daac7d369a582c2adcccebf155bd6
Arthur <villardarthur@gmail.com>
2024-06-13 19:13:24 +0200

    Fix server path

--- server/src/server.ts
+++ server/src/server.ts
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json())
 
-app.use("/api", EntryPoint);
+app.use("/", EntryPoint);
 
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
