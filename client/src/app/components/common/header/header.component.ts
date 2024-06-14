f87c4dc74aff34663c1bca4f5ea0752e7fe09608
Arthur <94467283+avlrd@users.noreply.github.com>
2024-06-14 13:24:33 +0000

    Added some responsive and font awesome icons

--- client/src/app/components/common/header/header.component.ts
+++ client/src/app/components/common/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { IconDefinition, faBagShopping, faCartShopping, faUser, faBars } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
 	selector: 'app-header',
@@ -7,9 +9,13 @@ import { RouterLink } from '@angular/router';
 	templateUrl: './header.component.html',
 	styleUrl: './header.component.css',
 	imports: [
-		RouterLink
+		RouterLink,
+		FontAwesomeModule
 	]
 })
 export class HeaderComponent {
-	
+	faBagShopping: IconDefinition = faBagShopping;
+	faCartShopping: IconDefinition = faCartShopping;
+	faUser: IconDefinition = faUser;
+	faBars: IconDefinition = faBars;
 }
